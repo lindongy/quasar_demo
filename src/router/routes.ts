2dbcdc5dcc36f7ce4c65f1f09a48b27cd17bb8df
Sebastien Rinsoz <sebastien@yoctopuce.com>
2023-01-10 09:24:33 +0100

    Switch to Vite packager

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,18 +4,15 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '/', component: () => import('pages/IndexPage.vue') },
-      { path: '/settings', component: () => import('pages/Settings.vue') },
-      { path: '/about', component: () => import('pages/AboutPage.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ];
 
 export default routes;
